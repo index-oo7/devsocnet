9ecf5318541b0848ce302ae911466cd62038397e
teoo9 <teodorabrankovic01@gmail.com>
2023-03-27 11:44:15 +0200

    connecting edit to database v2

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -18,9 +18,9 @@ echo $response;
 }
 
 if($fun=="change"){
-if(isset($_POST['iduser']) and isset($_POST['name']) and isset($_POST['surname']) and isset($_POST['nickname']) and isset($_POST['info'])){
+if(isset($_POST['userid']) and isset($_POST['name']) and isset($_POST['surname']) and isset($_POST['nickname']) and isset($_POST['info'])){
     $db= new Database();
-    $iduser=$_POST['iduser'];
+    $iduser=$_POST['userid'];
     $name=$_POST['name'];
     $surname=$_POST['surname'];
     $nickname=$_POST['nickname'];
@@ -28,11 +28,14 @@ if(isset($_POST['iduser']) and isset($_POST['name']) and isset($_POST['surname']
     $query="SELECT * FROM app_user WHERE user_nickname='{$nickname}'";
     $res=mysqli_query($db->connect(),$query);
     if(mysqli_num_rows($res)==0){
-        $target_user="SELECT * FROM app_user WHERE user_id={$iduser}";
-        $response=$target_user->changeInfo($iduser,$name,$surname,$nickname,$info);
+        // $target_user="SELECT * FROM app_user WHERE user_id={$iduser}";
+        // $response=$target_user->changeInfo($iduser,$name,$surname,$nickname,$info);
+        $query="UPDATE app_user SET user_name='{$name}', user_surname='{$surname}', user_nickname='{$nickname}',user_info='{$info}' WHERE user_id={$iduser}";
+        $res=mysqli_query($db->connect(),$query);
+        $response="Data changed successfully!";
     }
     else{
-        $response="Nickname already exists. Please try with another one."
+        $response="Nickname already exists. Please try with another one.";
     }
 }    
 }
