c52bacee34b812d04dcc447cf11134d03910308b
index-oo7 <lukaradovanovic01@gmail.com>
2023-04-04 16:15:19 +0200

    Adding external links to body regarding to functionality

--- pages/Timeline/timeline.php
+++ pages/Timeline/timeline.php
@@ -16,22 +16,67 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Timeline</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Timeline</title>
 
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
-    <script src="https://use.fontawesome.com/1d73bb3427.js"></script>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="timeline.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+        <script src="https://use.fontawesome.com/1d73bb3427.js"></script>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="timeline.css">
 
-</head>
-<body>
-    <div id="searchBar"><input class="input" name="text" placeholder="Search..." type="search"></div>
-</body>
+    </head>
+    <body>
+        <div id="searchBar"><input class="input" name="text" placeholder="Search..." type="search"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  
+
+    <!-- Files handling ajax communication -->
+    <script src="../../scripts/jquery.js"></script>
+    <script src="../../scripts/ajaxcalls.js"></script>
+
+    <!-- Script file handling popup windows -->
+    <script src="script.js"></script>
+    </body>
 </html>
